1224d3a09d71eef8bffc8adf590c41cba91b3575
Dell <Dell@DESKTOP-QNNBM8A>
2021-04-23 20:32:46 +0100

    pushing project with login interface and registration

---
+++ OOP_CA_/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OOP_CA_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
