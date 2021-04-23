960cd9baa610d42e2845affead33553506f623f7
Dell <Dell@DESKTOP-QNNBM8A>
2021-04-23 20:37:04 +0100

    pushing work

--- OOP_CA_/Areas/Identity/IdentityHostingStartup.cs
+++ OOP_CA_/Areas/Identity/IdentityHostingStartup.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
